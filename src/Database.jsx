b98adf90fdadf252839985dbb1a593825ed6b80f
Lana Gomes <lana.gomes1919@gmail.com>
2024-06-26 19:44:50 -0300

    Adding first finished project

--- src/Database.jsx
+++ src/Database.jsx
@@ -1,26 +1,30 @@
 import capa from "./images/capa.png";
+import capaTimeSync from "./images/capaTimeSync.png";
 
 export const projects = [
   {
     id: 1,
-    projectImg: capa,
-    projectName: "Marka",
-    technologiesUsed: "VS Code |  Sublime Text |  Atom | iTerm2  | Hyper",
-    linkGithub: "https://github.com/LanaGomes",
+    projectImg: capaTimeSync,
+    projectName: "TimeSync",
+    technologiesUsed: " | Javascript | React | Tailwind | Vite ",
+    linkGithub: "https://github.com/LanaGomes/timeSync",
+    linkProject: "https://time-sync-sable.vercel.app/",
   },
   {
     id: 2,
     projectImg: capa,
     projectName: "Marka2",
     technologiesUsed: "VS Code | Sublime Text |  Atom | iTerm2 |  Hyper",
-    linkGithub: "https://github.com/LanaGomes",
+    linkGithub: "https://github.com/LanaGomes/portfolioLanaGomes",
+    linkProject: "https://github.com/LanaGomes/portfolioLanaGomes",
   },
 
   {
     id: 3,
     projectImg: capa,
     projectName: "Marka3",
-    technologiesUsed: "VS Code  Sublime Text  Atom iTerm2  Hyper",
-    linkGithub: "https://github.com/LanaGomes",
+    technologiesUsed: "VS Code |  Sublime Text |  Atom | iTerm2  | Hyper",
+    linkGithub: "https://github.com/LanaGomes/timeSync",
+    linkProject: "https://github.com/LanaGomes/portfolioLanaGomes",
   },
 ];
