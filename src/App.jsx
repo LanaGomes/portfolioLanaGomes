80afc0174feca2e69c1266c39059181ec6f6ac4d
Lana Gomes <lana.gomes1919@gmail.com>
2024-07-07 13:13:19 -0300

    Creating and making the flag Icons work

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
 import Contact from "./Components/Contact";
 
 import "./App.css";
-import Navbar from "./Components/Navbar";
 import About from "./Components/About";
 import Projects from "./Components/Projects";
+import { useTranslation } from "react-i18next";
+import "./i18n";
+import euaIcon from "./images/euaIcon.png";
+import brazIcon from "./images/brazilIcon.png";
 
 function App() {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
   return (
     <>
       <main className="mainContainer">
+        <section className="containerFlagIcon">
+          <div className="flagIcon" onClick={() => changeLanguage("en")}>
+            <img alt="BandeiraDosEstadosUnidos" src={euaIcon} />
+          </div>
+          <div className="flagIcon" onClick={() => changeLanguage("pt")}>
+            {" "}
+            <img alt="BandeiraDoBrasil" src={brazIcon} />
+          </div>
+        </section>
+
         <div className="containerAboutContact">
           {" "}
           <section className="transparentContainer1 ">
