a1de545cfa6702b4bc963722171468ae49f66de6
Lana Gomes <lana.gomes1919@gmail.com>
2024-07-09 11:35:43 -0300

    deleting component not useful

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import About from "./Components/About.jsx";
+
 import Contact from "./Components/Contact.jsx";
 const router = createBrowserRouter([
   {
